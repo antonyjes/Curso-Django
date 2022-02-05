43afc857d5e75288bb0d002fdd95df2f077c3aba
antonyjes <72809149@continental.edu.pe>
2022-02-04 20:21:14 -0500

    Creando la primera aplicación y primer modelo

--- PrimerProyecto/settings.py
+++ PrimerProyecto/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core.apps.CoreConfig',
+    #Apps
+    'core.erp'  #core/erp/apps.py -> name = 'core.erp'
 ]
 
 MIDDLEWARE = [
